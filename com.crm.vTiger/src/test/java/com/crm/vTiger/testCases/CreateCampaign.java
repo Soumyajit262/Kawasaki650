7b502527faac92f40ba640a48a03e04ad5dea5f2
Soumyajit262 <mandalsoumyajit262@gmail.com>
2022-03-23 17:28:01 +0530

    create campaign added

---
+++ com.crm.vTiger/src/test/java/com/crm/vTiger/testCases/CreateCampaign.java
@@ -0,0 +1,12 @@
+package com.crm.vTiger.testCases;
+
+import org.testng.annotations.Test;
+
+public class CreateCampaign {
+	@Test
+	public void createCampaign()
+	{
+		System.out.println("Campaign created successfully");
+	}
+
+}
