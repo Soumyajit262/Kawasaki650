d2a2d98666fb075fbf8497ce03e32bf8767879f8
Soumyajit262 <mandalsoumyajit262@gmail.com>
2022-03-28 14:45:50 +0530

    class name

--- com.crm.vTiger/src/test/java/com/crm/vTiger/testCases/CreateCampaignTest.java
+++ com.crm.vTiger/src/test/java/com/crm/vTiger/testCases/CreateCampaignTest.java
@@ -2,7 +2,7 @@ package com.crm.vTiger.testCases;
 
 import org.testng.annotations.Test;
 
-public class CreateCampaign {
+public class CreateCampaignTest {
 	@Test
 	public void createCampaign()
 	{
